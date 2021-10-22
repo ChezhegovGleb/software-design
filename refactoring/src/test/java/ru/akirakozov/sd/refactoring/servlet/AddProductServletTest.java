41653b86bf1a66d2924edcdde382c5f9643098f3
ChezhegovGleb <glebserov@list.ru>
2021-10-22 20:15:41 +0300

    Added tests for AddProductServlet

---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/AddProductServletTest.java
@@ -0,0 +1,118 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.mockito.Mockito.*;
+
+public class AddProductServletTest {
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private AddProductServlet servlet;
+    private StringWriter stringWriter;
+
+    @Before
+    public void setUp() {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        servlet = new AddProductServlet();
+        stringWriter = new StringWriter();
+    }
+
+    @Test
+    public void doGetSimpleTest() throws Exception {
+        when(request.getParameter("name")).thenReturn("iphone 7");
+        when(request.getParameter("price")).thenReturn("10000");
+        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+
+        servlet.doGet(request, response);
+
+        Assert.assertEquals("OK" + System.lineSeparator(), stringWriter.toString());
+
+        verify(response).setContentType("text/html");
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        verify(response).getWriter();
+    }
+
+    @Test
+    public void doGetMediumTest() throws Exception {
+        when(request.getParameter("name"))
+                .thenReturn("iphone 7")
+                .thenReturn("samsung");
+        when(request.getParameter("price"))
+                .thenReturn("10000")
+                .thenReturn("5000");
+        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+
+        servlet.doGet(request, response);
+        servlet.doGet(request, response);
+
+        Assert.assertEquals("OK" + System.lineSeparator()
+                + "OK" + System.lineSeparator(), stringWriter.toString());
+
+        verify(response, times(2)).setContentType("text/html");
+        verify(response, times(2)).setStatus(HttpServletResponse.SC_OK);
+        verify(response, times(2)).getWriter();
+    }
+
+    @Test
+    public void doGetWithNullNameTest() throws Exception {
+        when(request.getParameter("name")).thenReturn(null);
+        when(request.getParameter("price")).thenReturn("10000");
+        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+
+        servlet.doGet(request, response);
+
+        Assert.assertEquals("OK" + System.lineSeparator(), stringWriter.toString());
+
+        verify(response).setContentType("text/html");
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        verify(response).getWriter();
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void doGetWithNullPriceTest() throws Exception {
+        when(request.getParameter("name")).thenReturn("iphone 7");
+        when(request.getParameter("price")).thenReturn(null);
+        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+
+        servlet.doGet(request, response);
+    }
+
+    @Test
+    public void doGetWithEmptylNameTest() throws Exception {
+        when(request.getParameter("name")).thenReturn("");
+        when(request.getParameter("price")).thenReturn("10000");
+        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+
+        servlet.doGet(request, response);
+
+        Assert.assertEquals("OK" + System.lineSeparator(), stringWriter.toString());
+
+        verify(response).setContentType("text/html");
+        verify(response).setStatus(HttpServletResponse.SC_OK);
+        verify(response).getWriter();
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void doGetWithEmptylPriceTest() throws Exception {
+        when(request.getParameter("name")).thenReturn("iphone8");
+        when(request.getParameter("price")).thenReturn("");
+        when(response.getWriter()).thenReturn(new PrintWriter(stringWriter));
+
+        servlet.doGet(request, response);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void doGetWithNullTest() throws Exception {
+        servlet.doGet(null, null);
+    }
+
+}
