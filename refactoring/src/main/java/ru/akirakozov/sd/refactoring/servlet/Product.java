18a591a78302a2b8fdbdd6e2fc85912404b1bc31
ChezhegovGleb <glebserov@list.ru>
2021-10-25 00:34:59 +0300

    Added Product for storing product data

---
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/Product.java
@@ -0,0 +1,11 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+public class Product {
+    String name;
+    Long price;
+
+    Product(String name, Long price) {
+        this.name = name;
+        this.price = price;
+    }
+}
